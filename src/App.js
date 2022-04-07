902b02bbebb4d78a22787dfcb2796ba40bbb911b
masud rana <masudranaacb@gmail.com>
2022-04-08 05:01:54 +0600

    added bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import { getAuth } from "firebase/auth";
 import './App.css';
 import app from "./firebase.init";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+
 const auth = getAuth(app);
+
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handelEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  const handelPassword = (event) => {
+    setPassword(event.target.value)
+  }
+  const handelSubmit = (event) => {
+    console.log(email, password)
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      
+    <div >
+
+      <div className="w-50 mx-auto mt-3">
+        <Form onSubmit={handelSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handelEmail} type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handelPassword} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
