9695928f3df02a6105c5acd47642ca9608f47065
masud rana <masudranaacb@gmail.com>
2022-04-08 15:32:17 +0600

    added alerady register mathod

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import './App.css';
 import app from "./firebase.init";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,17 +8,22 @@ import { useState } from "react";
 const auth = getAuth(app);
 
 function App() {
+  const [register, setRegister] = useState(false)
   const [error, setError] = useState('')
   const [validated, setValidated] = useState(false);
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-// email,password handelar
+  // email,password handelar
   const handelEmail = (event) => {
     setEmail(event.target.value)
   }
   const handelPassword = (event) => {
     setPassword(event.target.value)
+  }
+  // checked handel 
+  const handelChecked = (event) => {
+    setRegister(event.target.checked)
   }
   // handel submit button 
   const handelSubmit = (event) => {
@@ -30,7 +35,7 @@ function App() {
       event.stopPropagation();
       return
     }
-    if(!/(?=.*[!@#$%^&*])/.test(password)){
+    if (!/(?=.*[!@#$%^&*])/.test(password)) {
       setError('please type one special(?=.*[!@#$%^&*])')
       return
     }
@@ -38,19 +43,38 @@ function App() {
     setValidated(true);
 
     // validation
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user)
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage)
-      });
+    if (register) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+          console.log(user)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage)
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setEmail('')
+          setPassword('')
+          console.log(user)
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          setError(errorMessage)
+          console.log(errorMessage)
+        });
+    }
+
   }
   return (
     <div >
+      <h2 className="text-center text-primary mt-4">Now {register ? 'Login' : "Register"}!!</h2>
       <div className="w-50 mx-auto mt-3">
         <Form noValidate validated={validated} onSubmit={handelSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -67,9 +91,12 @@ function App() {
               Please provide a valid city.....
             </Form.Control.Feedback>
             <p className=" text-danger">{error}</p>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check onChange={handelChecked} type="checkbox" label="Already Register?" />
+            </Form.Group>
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            {register ? 'Login' : "Submit"}
           </Button>
         </Form>
       </div>
